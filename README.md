57b8f7671e6223655f02fbc90bc8c52d3d46fca0
matt1883 <matthew.hicks@microsoft.com>
2017-12-01 13:26:26 -0800

    Adding PowerShell script and README

--- README.md
+++ README.md
@@ -1,57 +1,37 @@
-# Project Name
+---
+author: matt1883
+---
 
-(short, 1-3 sentenced, description of the project)
+# Data Lake Analytics - Using Azure PowerShell to get input and output paths of a U-SQL job
 
-## Features
+This script returns a list of input or output paths of a Data Lake Analytics U-SQL job. These paths can be any combination of U-SQL catalog paths, WASB file paths, or ADLS file paths.
 
-This project framework provides the following features:
+## Included script
 
-* Feature 1
-* Feature 2
-* ...
+* [/src/Get-AdlJobIoPath.ps1](/src/Get-AdlJobIoPath.ps1)
 
-## Getting Started
+## Prerequisite steps
 
-### Prerequisites
+1. Install [Azure PowerShell on a Windows machine](https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps)
+2. Open a new PowerShell session.
+3. Log in using ``Login-AzureRmAccount -SubscriptionId <SUBSCRIPTION-ID>``
 
-(ideally very short, if any)
+## How to run the script
 
-- OS
-- Library version
-- ...
+1. In PowerShell, navigate to the ``/src/`` folder.
+2. Run ``Get-AdlJobIoPath.ps1`` using the syntax below.
 
-### Installation
+**Syntax**
+```powershell
+Get-AdlJobIoPath.ps1 [-Account] <string> [-Id] <Guid> [-Direction] {Input | Output}
+```
 
-(ideally very short)
-
-- npm install [package name]
-- mvn install
-- ...
-
-### Quickstart
-(Add steps to get up and running quickly)
-
-1. git clone [repository clone url]
-2. cd [respository name]
-3. ...
-
-
-## Demo
-
-A demo app is included to show how to use the project.
-
-To run the demo, follow these steps:
-
-(Add steps to start up the demo)
-
-1.
-2.
-3.
+**Example**
+```powershell
+Get-AdlJobIoPath.ps1 -Account contosoadla -Id c3426e86-85f5-4521-b376-e4b3e8d32d8c -Direction Output
+```
 
 ## Resources
 
-(Any additional resources or related projects)
-
-- Link to supporting information
-- Link to similar sample
-- ...
+- [Learn more about Azure PowerShell](https://docs.microsoft.com/en-us/powershell/azure/overview)
+- [Learn more about Azure Data Lake Analytics](https://docs.microsoft.com/en-us/azure/data-lake-analytics/)
---
+++ src/Get-AdlJobIoPath.ps1
@@ -0,0 +1,93 @@
+﻿<#
+.SYNOPSIS
+Retrieves the input or output paths of a Data Lake Analytics U-SQL job.
+
+.DESCRIPTION
+This script returns a list of input or output paths of a Data Lake Analytics U-SQL job. These paths can be any combination of U-SQL catalog paths, WASB file paths, or ADLS file paths.
+Note: If you choose to depend on this script, please be aware that the structure of the file that this script reads could change in the future.
+
+.PARAMETER Account
+The name of the Data Lake Analytics account.
+
+.PARAMETER Id
+The Job ID of the job for which input or output paths should be returned. Find the Job ID by using the Get-AdlJob cmdlet or by opening the job in the portal.
+
+.PARAMETER Direction
+Indicates whether to return input or output paths. This can be "Input" or "Output".
+
+.EXAMPLE
+Get-AdlJobIoPath.ps1 -Account contosoadla -Id c3426e86-85f5-4521-b376-e4b3e8d32d8c -Direction Output
+#>
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string] $Account,
+    [Parameter(Mandatory=$true)]
+    [Guid] $Id,
+    [ValidateSet("Input", "Output")]
+    [Parameter(Mandatory=$true)]
+    [string] $Direction
+)
+
+try {
+    $isInput = $Direction -eq "Input"
+    
+    # Fetch algebra XML file path
+    $job = Get-AdlJob -Account $Account -JobId $Id
+    $algebraAdlsFullPath = [Uri]$job.Properties.AlgebraFilePath
+    $adlsAccountName = $algebraAdlsFullPath.Host.Split(".")[0]
+    $adlsPath = $algebraAdlsFullPath.AbsolutePath
+
+    # Open algebra XML file
+    $algebraXmlFileContents = Get-AdlStoreItemContent -Account $adlsAccountName -Path $adlsPath
+    $stages = ([xml]$algebraXmlFileContents).ScriptAndGraph.graph.process
+    
+    $paths = @()
+
+    # For each stage in the algebra XML file, identify the input or output paths
+    foreach ($stage in $stages) {
+        if ($isInput){
+            $items = $stage.input
+        }
+        else {
+            $items = $stage.output
+        }
+
+        foreach ($item in $items) {
+            if ($item.usqlTableName -ne $null) {
+                $newPath = New-Object -TypeName PSObject
+                $newPath | Add-Member -MemberType NoteProperty -Name Type -Value "Table"
+                $newPath | Add-Member -MemberType NoteProperty -Name Path -Value ($item.usqlTableName)
+                $paths += $newPath
+            }
+            elseif ($item.structuredStream -ne $null) {
+                $newPath = New-Object -TypeName PSObject
+                $newPath | Add-Member -MemberType NoteProperty -Name Type -Value "StructuredStream"
+                $newPath | Add-Member -MemberType NoteProperty -Name Path -Value ($item.structuredStream)
+                $paths += $newPath
+            }
+            elseif ($item.cosmosStream -ne $null) {
+                if ($item.cosmosStream.cosmosPath -ne $null) {
+                    foreach ($file in $item.cosmosStream) {
+                        $newPath = New-Object -TypeName PSObject
+                        $newPath | Add-Member -MemberType NoteProperty -Name Type -Value "File"
+                        $newPath | Add-Member -MemberType NoteProperty -Name Path -Value ($file.cosmosPath)
+                        $paths += $newPath
+                    }
+                }
+                else {
+                    $newPath = New-Object -TypeName PSObject
+                    $newPath | Add-Member -MemberType NoteProperty -Name Type -Value "File"
+                    $newPath | Add-Member -MemberType NoteProperty -Name Path -Value ($item.cosmosStream)
+                    $paths += $newPath
+                }
+            }
+        }
+    }
+
+    $paths | select -Property Type, Path -Unique
+}
+catch {
+    Write-Error $_
+}
